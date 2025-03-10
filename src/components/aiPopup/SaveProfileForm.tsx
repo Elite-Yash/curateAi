6e91931578a691c45a942a1023b663f617cb14c2
Dilipdabhi007 <dilip.elitesigma@gmail.com>
2025-03-10 18:48:37 +0530

    Scraping Data Form LinkedIn Page
    Scraping Data Form LinkedIn Page

---
+++ src/components/aiPopup/SaveProfileForm.tsx
@@ -0,0 +1,47 @@
+import { getImage } from "../../common/utils/logoUtils";
+
+// Define the props interface
+interface SaveProfileFormProps {
+    onClose: () => void; // Type the onClose prop as a function that returns void
+    profileName: string;
+    position: string;
+    company: string;
+}
+
+const SaveProfileForm: React.FC<SaveProfileFormProps> = ({ onClose, profileName, position, company }) => {
+    return (
+        <>
+            <div className="inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                <div className="popup-container bg-white shadow-lg w-96 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded-3xl overflow-hidden w-400">
+                    <div className="header-top p-9 py-10 flex justify-between item-center">
+                        <h4 className="popup-title font-semibold text-xl">Save Profile</h4>
+                        <span onClick={onClose} className="close-box absolute top-6 right-6 w-6 h-6 bg-no-repeat bg-center cursor-pointer">
+                            <img src={getImage('close')} alt="img" className="w-full h-full rounded-full" />
+                        </span>
+                    </div>
+                    <div className="p-9 flex justify-between item-center flex-col gap-5">
+                        <div className="w-full input-group">
+                            <input type="text" className="popup-input w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200" placeholder="Name" value={profileName} />
+                        </div>
+                        <div className="w-full input-group">
+                            <input type="email" className="popup-input w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200" placeholder="Email" />
+                        </div>
+                        <div className="w-full input-group">
+                            <input type="text" className="popup-input w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200" placeholder="Position" value={position} />
+                        </div>
+                        <div className="w-full input-group">
+                            <input type="text" className="popup-input w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200" placeholder="Company/Institution" value={company} />
+                        </div>
+                        <div className="popup-buttons justify-end space-x-2 text-right relative">
+                            <button className="justify-center flex gap-2 ml-auto leading-6 popup-button-submit px-4 py-2 bg-gradient-to-r from-purple-500 to-blue-500 text-white rounded-md hover:bg-indigo-600 disabled:bg-gray-400" style={{ "width": "80px" }}>
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SaveProfileForm;
--- src/injector/LinkedInProfile.tsx
+++ src/injector/LinkedInProfile.tsx
@@ -5,23 +5,59 @@ import { createCustomButton } from "../common/utils/createCustomButton";
 
 const LinkedInProfile = () => {
     const [openAiPopup, setOpenAiPopup] = useState(false);
+    const [profileName, setProfileName] = useState("");
+    const [email, setEmail] = useState("");
+    const [position, setPosition] = useState("");
+    const [company, setCompany] = useState("");
+
+    const scrapeProfileData = () => {
+        const profileNameElement = document.querySelector("div.ph5 div.mt2.relative h1");
+        const experienceSection = document.querySelector("section.artdeco-card.pv-profile-card.break-words.mt2 div#experience")?.closest("section");
+        const experienceData = experienceSection?.lastElementChild;
+        const positionElement = experienceData?.querySelector("div.display-flex.flex-column .t-bold span");
+        const companyElement = experienceData?.querySelector("div.display-flex.flex-column .t-14.t-normal span");
+        const profileNameText = profileNameElement?.textContent.trim() || "";
+        const positionText = positionElement?.textContent.trim() || "";
+        const companyText = companyElement?.textContent.trim() || "";
+
+        const cleanCompanyText = companyText.split("·")[0].trim();
+        if (companyText.includes("yrs") || companyText.includes("mos") || companyText.includes("mo")) {
+            const position = experienceData?.querySelector("div.pvs-entity__sub-components ul li div.display-flex.flex-column.align-self-center.flex-grow-1 div.display-flex.full-width span");
+            setPosition(position?.textContent.trim() || "");
+            setCompany(positionText);
+            setProfileName(profileNameText);
+        } else {
+            setProfileName(profileNameText);
+            setPosition(positionText);
+            setCompany(cleanCompanyText);
+        }
+    };
+
+    const handleClick = () => {
+        scrapeProfileData();
+        setOpenAiPopup(true);
+    };
 
     useEffect(() => {
-        const spans = document.querySelector("div.ph5 div.mt2.relative").querySelectorAll("span");
-        if (spans.length > 0) {
+        const spans = document.querySelector("div.ph5 div.mt2.relative")?.querySelectorAll("span");
 
-            const customeSpan = createCustomButton('Save', getImage("saveProfileIcon"), 'Save Profile');
-            // Append main span to the closest div
-            spans[0].closest("div").appendChild(customeSpan);
+        if (spans?.length > 0) {
+            const customSpan = createCustomButton('Save', getImage("saveProfileIcon"), 'Save Profile');
+            spans[0].closest("div").appendChild(customSpan);
 
-            // Add click event listener to open the form popup
-            customeSpan.addEventListener("click", openFormPopup);
+            // Add event listener to the button
+            customSpan.addEventListener("click", handleClick);
+
+            // Cleanup function to remove event listener
+            return () => {
+                customSpan.removeEventListener("click", handleClick);
+            };
         }
-    }, []); // Empty dependency array to run once after mount
+    }, []);
 
-    const openFormPopup = () => {
-        setOpenAiPopup(true);
-    }
+    // console.log("Profile Name: ", profileName);
+    // console.log("Position: ", position);
+    // console.log("Company: ", company);
 
     if (openAiPopup) {
         return (
@@ -30,7 +66,6 @@ const LinkedInProfile = () => {
                     position: "fixed",
                     width: "100%",
                     height: "100vh",
-                    maxHeight: "100vh",
                     backgroundColor: "rgba(0, 0, 0, 0.3)",
                     top: 0,
                     left: 0,
@@ -40,7 +75,7 @@ const LinkedInProfile = () => {
                     zIndex: 21213123,
                 }}
             >
-                <SaveProfileForm onClose={() => setOpenAiPopup(false)} />
+                <SaveProfileForm onClose={() => setOpenAiPopup(false)} profileName={profileName} position={position} company={company} />
             </div>
         );
     }
